fa5a81f06b199079c64c74e314b18f01fce977c8
kantanakayama <sonar000851@gmail.com>
2024-06-21 11:07:13 +0900

    ボタンアコーディオン追加

--- resources/ts/pages/home.tsx
+++ resources/ts/pages/home.tsx
@@ -13,39 +13,64 @@ const Home: React.FC = () => {
   // ユーザー情報を格納するための変数を定義
   const [user, setUser] = useState<UserProps | null>(null);
 
+  // ボタン押下時に切り替えを管理する
+  const [reportsButton, setReportsButton] = useState<boolean>(false);
+  const [dawnLoadButton, setDawnLoadButton] = useState<boolean>(false);
+
+  // ボタンを押した際に表示の切り替えを行う
+  const clickButton = (which:boolean) => {
+    if(which){
+      setReportsButton(reportsButton ? false : true)
+      setDawnLoadButton(false)
+    } else {      
+      setReportsButton(false)
+      setDawnLoadButton(dawnLoadButton ? false : true)
+    }
+  }
+
+  // 表示の切り替えを行うためのcssオブジェクト
+  const reportsButtonStyle = {
+    display: reportsButton  ? 'block' : 'none',
+  };
+  const dawnloadButtonStyle = {
+    display: dawnLoadButton  ? 'block' : 'none',
+  };
+
+
+
   // 画面が読み込まれたら実行する処理：APIを呼び出してユーザー情報を取り出す
-	useEffect(() => {
-		const fetchUser = async () => {
-			try {
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
         // csfrトークンを取得してヘッダーに追加する
         const csrfToken = getCsrfToken();
         axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken;
         // エンドポイントからAPI呼び出し格納する
-				const response = await axios.get(rootConst.GETUSERAPI);
-				const data = response.data;
-				// 必要なデータを整形
-				const user:UserProps = {
-					id: data.id,
-					name: data.name,
-					Department: data.Department,
-					checkAuth: data.checkAuth,
-					adminAuth: data.adminAuth,
-				};
-				setUser(user);
-			} catch (error) {
-				console.error('ユーザー情報の取得に失敗しました:', error);
-			}
-		};
+        const response = await axios.get(rootConst.GETUSERAPI);
+        const data = response.data;
+        // 必要なデータを整形
+        const user: UserProps = {
+          id: data.id,
+          name: data.name,
+          Department: data.Department,
+          checkAuth: data.checkAuth,
+          adminAuth: data.adminAuth,
+        };
+        setUser(user);
+      } catch (error) {
+        console.error('ユーザー情報の取得に失敗しました:', error);
+      }
+    };
 
-		fetchUser();
-	}, []);
+    fetchUser();
+  }, []);
 
   // ボタンコンポーネントに渡す用のプロパティを設定する
   const reportsPostBtn: ButtonProps = {
     label: '週報提出',
     checkAuth: true,
     adminAuth: true,
-    classPro: 'btn btn-success mb-2 buttonW',
+    classPro: 'btn btn-success-sub mb-2 buttonW',
     fontAwesome: 'fa-solid fa-pen-to-square',
   };
 
@@ -53,7 +78,7 @@ const Home: React.FC = () => {
     label: '週報確認',
     checkAuth: true,
     adminAuth: true,
-    classPro: 'btn btn-success mb-2 buttonW',
+    classPro: 'btn btn-success-sub mb-2 buttonW',
     fontAwesome: 'fa-solid fa-file-import',
   };
 
@@ -61,8 +86,8 @@ const Home: React.FC = () => {
     label: '週報確認（管理者）',
     checkAuth: user?.checkAuth,
     adminAuth: true,
-    classPro: 'btn btn-success mb-2 buttonW',
-    fontAwesome: 'fa-solid fa-file-import',
+    classPro: 'btn btn-success-sub mb-2 buttonW',
+    fontAwesome: 'fa-regular fa-eye',
   };
 
   const AdminBtn: ButtonProps = {
@@ -74,23 +99,23 @@ const Home: React.FC = () => {
   };
 
   const reportsPostPrm: HttpRequestProps = {
-    requestURL:rootConst.REPORTSPOSTAPI,
-    redirectURL:'/klug_app/public/reportsPost'
+    requestURL: rootConst.REPORTSPOSTAPI,
+    redirectURL: '/klug_app/public/reportsPost'
   };
 
   const reportsCheckPrm: HttpRequestProps = {
-    requestURL:rootConst.REPORTSCHECKAPI,
-    redirectURL:'/klug_app/public/reportsCheck'
+    requestURL: rootConst.REPORTSCHECKAPI,
+    redirectURL: '/klug_app/public/reportsCheck'
   };
 
   const reportsCheckAdminPrm: HttpRequestProps = {
-    requestURL:rootConst.REPORTSCHECKADMIN,
-    redirectURL:'/klug_app/public/reportsCheckAdmin'
+    requestURL: rootConst.REPORTSCHECKADMIN,
+    redirectURL: '/klug_app/public/reportsCheckAdmin'
   };
 
   const adminPrm: HttpRequestProps = {
-    requestURL:rootConst.ADMIN,
-    redirectURL:'/klug_app/public/admin'
+    requestURL: rootConst.ADMIN,
+    redirectURL: '/klug_app/public/admin'
   };
 
   return (
@@ -99,25 +124,33 @@ const Home: React.FC = () => {
       <div className="wrapper">
         <main>
           <UserInfo user={user?.name ?? ""} Department={user?.Department ?? ""} classPro="bg-success text-white" />
-          <Button
-           ButtonProps={reportsPostBtn}
-           HttpRequestProps={reportsPostPrm}
-          />
-          <br />
-          <Button
-          ButtonProps={reportsCheckBtn}
-          HttpRequestProps={reportsCheckPrm}
-          />
-          <br />
-          <Button
-          ButtonProps={reportsCheckAdminBtn}
-          HttpRequestProps={reportsCheckAdminPrm}
-          />
-          <br />
-          <Button
-          ButtonProps={AdminBtn}
-          HttpRequestProps={adminPrm}
-          />
+          <button onClick={() => clickButton(true)} className='btn btn-success mb-2 buttonW'><i className='fa-solid fa-pen-to-square'>週報関係</i></button>
+            <div style={reportsButtonStyle} className='ml-4'>
+              <Button
+                ButtonProps={reportsPostBtn}
+                HttpRequestProps={reportsPostPrm}
+              />
+              <br />
+              <Button
+                ButtonProps={reportsCheckBtn}
+                HttpRequestProps={reportsCheckPrm}
+              />
+              <br />
+              <Button
+                ButtonProps={reportsCheckAdminBtn}
+                HttpRequestProps={reportsCheckAdminPrm}
+              />
+              <br />
+            </div>
+          <button onClick={() => clickButton(false)} className='btn btn-info mb-2 buttonW'><i className="fa-solid fa-download">資料ダウンロード</i></button>
+          <div style={dawnloadButtonStyle} className='ml-4'>
+          <button className='btn btn-info-sub mb-2 buttonW'><i className="fa-solid fa-inbox"></i>届出書</button>
+
+          </div>
+              <Button
+                ButtonProps={AdminBtn}
+                HttpRequestProps={adminPrm}
+              />
           <Outlet />
         </main>
       </div>
