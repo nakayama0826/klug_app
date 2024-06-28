31861c3d2de7b93e23e09d26e136c2b98c3ae87d
kantanakayama <sonar000851@gmail.com>
2024-06-28 23:50:12 +0900

    https対応

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 
 use Illuminate\Pagination\Paginator;
 
@@ -27,5 +28,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+
+        if (App::environment('production','staging')) {
+            URL::forceScheme('https');
+        }
     }
 }
