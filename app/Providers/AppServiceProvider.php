451982d2e02576046c3a70cbdcb42b5cc61b5f98
kantanakayama <sonar000851@gmail.com>
2024-06-29 00:00:45 +0900

    URL修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,9 +27,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (App::environment('production','staging')) {
-            URL::forceScheme('https');
-        }
+        URL::forceScheme('https');
 
         Paginator::useBootstrap();
     }
